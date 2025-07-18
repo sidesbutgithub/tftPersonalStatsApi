d0fb07080803a1cbf78e438762b7255784713d5c
sidesbutgithub <SidneyWai3@gmail.com>
2025-07-18 04:35:19 -0400

    made requsts for match data asynchronous

--- tftStats/personalStats/utils.py
+++ tftStats/personalStats/utils.py
@@ -3,6 +3,8 @@ from django.core.cache import cache
 import requests
 from dotenv import dotenv_values
 from rest_framework.response import Response
+import asyncio
+import aiohttp
 
 async def getPuuid(request, next, **kwargs):
     puuid = cache.get(f"{kwargs['playerID']}#{kwargs['playerTag']}")
@@ -32,21 +34,27 @@ async def getPlayerLast20(request, **kwargs):
     last20 = []
     units = {}
 
-    for i in matches:
-        reqUrl = f"https://{kwargs['region']}.api.riotgames.com/tft/match/v1/matches/{i}?api_key={dotenv_values(".env")["RIOT_API_KEY"]}"
-        res = requests.get(reqUrl)
-        res.raise_for_status()
-        matchParticipants = res.json()['info']['participants']
-        for j in matchParticipants:
-            if j['puuid'] == request.puuid:
-                last20.append(j['placement'])
-                for unit in j['units']:
-                    unitName = f"{unit["character_id"]}-{unit["tier"]}"
-                    if not(unitName in units):
-                        units[unitName] = {"games": 0, "totalPlacement": 0}
-                    units[unitName]['games'] += 1
-                    units[unitName]['totalPlacement'] += j['placement']
-                break
+    requestUrls = [f"https://{kwargs['region']}.api.riotgames.com/tft/match/v1/matches/{i}?api_key={dotenv_values(".env")["RIOT_API_KEY"]}" for i in matches]
+
+    async with aiohttp.ClientSession() as session:
+        tasks = [getMatchStats(session, url, request.puuid) for url in requestUrls]
+        results = await asyncio.gather(*tasks)
+        for i in results:
+            for participant in i:
+                if participant['puuid'] == request.puuid:
+                    last20.append(participant['placement'])
+                    for unit in participant['units']:
+                        unitName = f"{unit["character_id"]}-{unit["tier"]}"
+                        if not(unitName in units):
+                            units[unitName] = {"games": 0, "totalPlacement": 0}
+                        units[unitName]['games'] += 1
+                        units[unitName]['totalPlacement'] += participant['placement']
+                    break
 
     cache.set(f"{kwargs['playerID']}#{kwargs['playerTag']}_data", json.dumps({"scores": last20, "units": units})) #default of 5 minute ttl
     return Response({"scores": last20, "units": units})
+
+async def getMatchStats(session, url, puuid):
+    async with session.get(url) as response:
+        res = await response.json()
+        return res['info']['participants']
